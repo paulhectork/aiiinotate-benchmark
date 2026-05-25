609291dfb8828a93790e6066da015f0e2775aa19
paulhectork <paul.kervegan@hotmail.fr>
2026-05-25 13:32:11 +0200

    [CONSTANTS] redo benchmark steps

--- src/constants.py
+++ src/constants.py
@@ -12,51 +12,95 @@ PATH_OUT = PATH_ROOT / "out"
 
 #NOTE: on the cli-side, seps are organised in grops: STEPS_GROUPS (1 group / number of manifests).
 # in the code-side, steps are not grouped: see STEPS_FLAT.
-# list of list of [n_canvas, n_manifest]
-STEPS_GROUP = [
-    # [
-    #     [10, 10],
-    #     [10,100],
-    #     [10,100],
-    # ],
-    [
-        [100, 100],
-        [100, 1000],
-        [100, 10000],
-    ],
-    [
-        [1000, 100],
-        [1000, 1000],
-        [1000, 10000],
-    ],
-    [
-        [10000, 100],
-        [10000, 1000],
-        [10000, 10000],
-    ],
-    [
-        [100000, 100],
-        [100000, 1000],
-        [100000, 10000],
-    ],
-    [
-        [1000000, 100],
-        [1000000, 1000],
-        [1000000, 10000],
-    ]
+# list of [n_manifest, n_canvas, n_annotations]
+# max #ann = 10K (to be realistic)
+
+# canvas-ot-annotation ratio (in 0..1 rangw): insert annotations on 10% of canvases
+# on one production instance of aiiinotate, RATIO is closer to 0.4. see notes below.
+RATIO = 0.1
+
+# steps of the benchmark, defined as a list of
+# ```
+# (n_manifest, n_canvases_per_manifest)
+# ```
+#
+# the table below summarizes the number of manifests, canvases and annotations in the
+# entire database at each step. the formulas used to get the values are:
+# ```
+# >>> total_canvases    = n_manifest * n_annotations
+# >>> total_annotations = n_manifest * n_annotations * RATIO
+# ```
+#
+#    manifests   canvases/manifest   total canvases    annotations
+# ----------------------------------------------------------------
+#            1             100                  100             10
+#            1           1,000                1,000            100
+#           10           1,000               10,000          1,000
+#          100           1,000              100,000         10,000
+#        1,000           1,000            1,000,000        100,000
+#       10,000           1,000           10,000,000      1,000,000
+#      100,000           1,000          100,000,000     10,000,000
+#    1,000,000           1,000        1,000,000,000    100,000,000
+#
+# we have MUCH more canvases than annotations in the DB: annotations and
+# images are on a small subset of all the pages of a document. depending on RATIO,
+# this can vary (canvas-to-annotation ratio = 1/RATIO).
+#
+# to verify the actual RATIO on a production database,
+# 1. get the total number of canvas with the query:
+# ```
+# >>> db.manifests2.aggregate([ {
+# ...   $group: {
+# ...       _id: null,
+# ...       n_canvas_total: { $sum: { $size: "$canvasIds" }
+# ...   }
+# ... }]);
+# ```
+# 2. get the total number of annotations with:
+# ```
+# >>> db.annotations2.countDocuments();
+# ```
+# 3. get the actual ratio with:
+# ```
+# >>> RATIO = n_annotations / n_canvas_total
+# ```
+STEPS = [
+    (1,           100),   #       1   ×   100 × 0.1 =          10
+    (1,         1_000),   #       1   × 1_000 × 0.1 =         100
+    (10,        1_000),   #      10   × 1_000 × 0.1 =       1_000
+    (100,       1_000),   #     100   × 1_000 × 0.1 =      10_000
+    (1_000,     1_000),   #   1_000   × 1_000 × 0.1 =     100_000
+    (10_000,    1_000),   #  10_000   × 1_000 × 0.1 =   1_000_000
+    (100_000,   1_000),   # 100_000   × 1_000 × 0.1 =  10_000_000
+    (1_000_000, 1_000),   # 1_000_000 × 1_000 × 0.1 = 100_000_000
 ]
-
-# used to define n_steps in cli
-STEPS_GROUP_RANGE = [1, len(STEPS_GROUP)]
-
-# flattened variant of the above, that will actually be used.
-STEPS_FLAT = list(chain.from_iterable(STEPS_GROUP))
-
-# number of step groups to run in the benchmark
-N_STEPS_DEFAULT = 2
+# STEPS = [
+#     [10,    10],
+#     [10,    100],
+#     [100,   10],
+#     [100,   100],
+#     [1_000,  100],
+#     [100,   1_000],
+#     [1_000,  1_000],
+#     [1_000,  10_000],
+#     [10_000, 1_000],
+#     [10_000, 10_000],
+#     [10_000, 100_000],
+#     [100_000, 100_000]
+#     # [10000, 100],
+#     # [100, 1000],
+#     # [1000, 1000],
+#     # [10000, 1000],
+#     # [100, 10000],
+#     # [1000, 10000],
+#     # [10000, 10000],
+#     # [100, 100000],
+#     # [1000, 100000],
+#     # [10000, 100000],
+#     # [100, 1000000],
+#     # [1000, 1000000],
+#     # [10000, 1000000],
+# ]
 
 # default number of threads to use
 THREADS_DEFAULT = 20
-
-# canvases with annotations / canvases without annotations
-RATIO_DEFAULT= 0.01
